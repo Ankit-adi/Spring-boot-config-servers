a7b1c476fd2c5afb082ddf2380dc437b215c4f42
Ankit Aditya <Ankit.Aditya@BRILLIO.COM>
2023-05-11 12:43:36 +0530

    Initial commit

---
+++ src/main/java/com/brillio/configServer/SpringBootConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.brillio.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brillio/configServer/SpringBootConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brillio.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
